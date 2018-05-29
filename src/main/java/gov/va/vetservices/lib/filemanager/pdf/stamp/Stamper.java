fabb48437a426272efc5a3c0b7b9d0d6417812d1
armin <aburkholder@meetveracity.com>
2018-05-29 11:22:38 -0400

    ds-49 This commit is ONLY for sharing ManualPdfTester unit test of iText 7 research. The project is otherwise broken due to iText 7 API.

--- src/main/java/gov/va/vetservices/lib/filemanager/pdf/stamp/Stamper.java
+++ src/main/java/gov/va/vetservices/lib/filemanager/pdf/stamp/Stamper.java
@@ -1,6 +1,6 @@
 package gov.va.vetservices.lib.filemanager.pdf.stamp;
 
-import java.awt.Color;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.text.MessageFormat;
 
@@ -8,20 +8,21 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.lowagie.text.Chunk;
-import com.lowagie.text.Phrase;
-import com.lowagie.text.Rectangle;
-import com.lowagie.text.pdf.ColumnText;
-import com.lowagie.text.pdf.PdfContentByte;
-import com.lowagie.text.pdf.PdfReader;
-import com.lowagie.text.pdf.PdfStamper;
+import com.itextpdf.io.font.constants.StandardFonts;
+import com.itextpdf.kernel.colors.DeviceCmyk;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.geom.Rectangle;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfPage;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
 
 import gov.va.ascent.framework.messages.MessageSeverity;
 import gov.va.vetservices.lib.filemanager.api.v1.transfer.FileDto;
 import gov.va.vetservices.lib.filemanager.exception.PdfStamperException;
 import gov.va.vetservices.lib.filemanager.impl.dto.DocMetadataDto;
 import gov.va.vetservices.lib.filemanager.impl.validate.MessageKeysEnum;
-import gov.va.vetservices.lib.filemanager.pdf.font.PdfFontFactory;
 import gov.va.vetservices.lib.filemanager.pdf.stamp.dto.StampDataDto;
 
 /**
@@ -48,44 +49,67 @@ public class Stamper {
 			throws PdfStamperException {
 
 		final ByteArrayOutputStream pdf = new ByteArrayOutputStream();
+		ByteArrayInputStream bais = null;
 
-		if ((metadata == null) || (stampDataDto == null) || (fileDto == null)) {
+		if (metadata == null || stampDataDto == null || fileDto == null) {
 			LOGGER.error("Arguments cannot be null.");
 			throw new IllegalArgumentException(
 					"Arguments to " + this.getClass().getSimpleName() + ".stamp(..) cannot be null or empty.");
 		}
 
-		PdfReader pdfReader = null;
-		PdfStamper pdfStamper = null;
+		PdfDocument pdfDoc = null;
+//		PdfReader pdfReader = null;
+//		PdfStamper pdfStamper = null;
 		try {
-			pdfReader = new PdfReader(fileDto.getFilebytes());
-			pdfReader.setAppendable(true); // make sure we can append
-
-			pdfStamper = new PdfStamper(pdfReader, pdf);
-			String stampText = StampsEnum.getStampText(stampDataDto.getProcessType(), metadata.getClaimId());
-
-			final ColumnText columnText = new ColumnText(null);
-			for (int pageNum = 1; pageNum <= pdfReader.getNumberOfPages(); pageNum++) {
-				final Rectangle rect = pdfReader.getPageSize(pageNum);
-				final Chunk textAsChunk = new Chunk(stampText, PdfFontFactory.getFont(stampDataDto.getFontName()));
-				textAsChunk.setBackground(new Color(255, 255, 255), 5f, 5f, 25f, 10f);
-
-				ColumnText.showTextAligned(pdfStamper.getOverContent(pageNum), PdfContentByte.ALIGN_LEFT, new Phrase(textAsChunk), 5f,
-						rect.getHeight() - stampDataDto.getFontSizeInPoints(), 0, 0, 0);
+//			pdfReader = new PdfReader(fileDto.getFilebytes());
+			bais = new ByteArrayInputStream(fileDto.getFilebytes());
+			final PdfReader pdfReader = new PdfReader(bais);
+			pdfReader.setUnethicalReading(true);
+			final PdfWriter pdfWriter = new PdfWriter(pdf);
+			pdfDoc = new PdfDocument(pdfReader, pdfWriter);
+//			pdfReader.setAppendable(true); // make sure we can append
+			if (!pdfDoc.isAppendMode()) {
+				// NOSONAR TODO look into adding ReaderProperties to pdfReader = new PdfReader(bais, ...)
 			}
-			while (true) {
-				if (!ColumnText.hasMoreText(columnText.go())) {
-					break;
-				}
+
+//			pdfStamper = new PdfStamper(pdfReader, pdf);
+			final String stampText = StampsEnum.getStampText(stampDataDto.getProcessType(), metadata.getClaimId());
+
+//			final ColumnText columnText = new ColumnText(null);
+//			for (int pageNum = 1; pageNum <= pdfReader.getNumberOfPages(); pageNum++) {
+			Rectangle rect = null;
+			PdfCanvas canvas = null;
+			for (int pageNum = 1; pageNum <= pdfDoc.getNumberOfPages(); pageNum++) {
+//				final Rectangle rect = pdfReader.getPageSize(pageNum);
+				final PdfPage page = pdfDoc.getPage(pageNum);
+				rect = page.getPageSize();
+				canvas = new PdfCanvas(page);
+//				final Chunk textAsChunk = new Chunk(stampText, PdfFontFactory.getFont(stampDataDto.getFontName()));
+//				textAsChunk.setBackground(new Color(255, 255, 255), 5f, 5f, 25f, 10f);
+
+//				ColumnText.showTextAligned(pdfStamper.getOverContent(pageNum), PdfContentByte.ALIGN_LEFT, new Phrase(textAsChunk), 5f,
+//						rect.getHeight() - stampDataDto.getFontSizeInPoints(), 0, 0, 0);
+				canvas.beginText()
+						// NOSONAR TODO would need to change font definition in StampDataDto
+						.setFontAndSize(PdfFontFactory.createFont(StandardFonts.COURIER),
+								Float.parseFloat(Integer.toString(stampDataDto.getFontSizeInPoints())))
+						.setFillColor(new DeviceCmyk(100, 100, 100, 0))
+						.moveText(5, rect.getHeight() - stampDataDto.getFontSizeInPoints()).showText(stampText).endText();
 			}
+//			while (true) {
+//				if (!ColumnText.hasMoreText(columnText.go())) {
+//					break;
+//				}
+//			}
 		} catch (final Throwable e) { // NOSONAR intentionally catching everything
-			MessageKeysEnum mke = MessageKeysEnum.PDF_STAMPING;
-			String msg = MessageFormat.format(mke.getMessage(), fileDto.getFilename(),
+			final MessageKeysEnum mke = MessageKeysEnum.PDF_STAMPING;
+			final String msg = MessageFormat.format(mke.getMessage(), fileDto.getFilename(),
 					e.getClass().getSimpleName() + " - " + StringUtils.substringBefore(e.getMessage(), "\n"));
 			LOGGER.error(mke.getKey() + ": " + msg, e);
 			throw new PdfStamperException(e, MessageSeverity.ERROR, mke.getKey(), msg);
 		} finally {
-			close(pdfStamper, pdfReader);
+//			close(pdfStamper, pdfReader);
+			close(pdfDoc);
 		}
 		return pdf.toByteArray();
 	}
@@ -96,20 +120,30 @@ public class Stamper {
 	 * @param pdfStamper the stamper.
 	 * @param pdfReader the pdfReader.
 	 */
-	private void close(final PdfStamper pdfStamper, final PdfReader pdfReader) {
-		try {
-			if (pdfStamper != null) {
-				pdfStamper.close();
+//	private void close(final PdfStamper pdfStamper, final PdfReader pdfReader) {
+	private void close(final PdfDocument pdfDoc) {
+		if (pdfDoc != null) {
+			pdfDoc.setCloseReader(true);
+			pdfDoc.setCloseWriter(true);
+			try {
+				pdfDoc.close();
+			} catch (final Exception e) {
+				LOGGER.debug("pdfDoc.close() failed.", e);
 			}
-		} catch (final Exception e) { // NOSONAR - intentionally catching generic exception
-			LOGGER.error("pdfStamper.close() failed", e);
 		}
-		try {
-			if (pdfReader != null) {
-				pdfReader.close();
-			}
-		} catch (final Exception e) { // NOSONAR - intentionally catching generic exception
-			LOGGER.error("pdfReader.close() failed", e);
-		}
+//		try {
+//			if (pdfStamper != null) {
+//				pdfStamper.close();
+//			}
+//		} catch (final Exception e) { // NOSONAR - intentionally catching generic exception
+//			LOGGER.error("pdfStamper.close() failed", e);
+//		}
+//		try {
+//			if (pdfReader != null) {
+//				pdfReader.close();
+//			}
+//		} catch (final Exception e) { // NOSONAR - intentionally catching generic exception
+//			LOGGER.error("pdfReader.close() failed", e);
+//		}
 	}
 }
